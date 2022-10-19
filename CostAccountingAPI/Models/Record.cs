d86889386dbd83841b8996e1d7bbb9a816d2d14a
juliion <juliaionova111@gmail.com>
2022-10-19 14:37:08 +0300

    Add Models
    Create classes User, Category, Record

---
+++ CostAccountingAPI/Models/Record.cs
@@ -0,0 +1,10 @@
+﻿namespace CostAccountingAPI.Models;
+
+public class Record
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int CategoryId { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Costs { get; set; }
+}
