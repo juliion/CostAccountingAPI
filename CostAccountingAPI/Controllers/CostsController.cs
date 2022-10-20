3723a5fe20dcdf1fc50fd0741dcd0ff1b489d2fc
juliion <juliaionova111@gmail.com>
2022-10-20 11:50:22 +0300

    Add CostsController
    Controller to handle all requests, connect by dependency injection DataService

---
+++ CostAccountingAPI/Controllers/CostsController.cs
@@ -0,0 +1,16 @@
+﻿using CostAccountingAPI.Models;
+using CostAccountingAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CostAccountingAPI.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class CostsController : ControllerBase
+{
+    private readonly IDataService _dataService; 
+    public CostsController(IDataService dataService)
+    {
+        _dataService = dataService;
+    }
+}
