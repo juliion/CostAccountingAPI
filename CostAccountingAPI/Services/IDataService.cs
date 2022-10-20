c6c8aee03bf38640b1a5145824485911c9eae82b
juliion <juliaionova111@gmail.com>
2022-10-20 11:44:53 +0300

    Add interface IDataService to store lists of data

---
+++ CostAccountingAPI/Services/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using CostAccountingAPI.Models;
+
+namespace CostAccountingAPI.Services;
+
+public interface IDataService
+{
+    public List<User> Users { get; }
+    public List<Category> Categories { get; }
+    public List<Record> Records { get; }
+}
