c8aca437ce9d0747241bea46a5530f66ae436879
juliion <juliaionova111@gmail.com>
2022-10-20 11:45:47 +0300

    Add implementation IDataService

---
+++ CostAccountingAPI/Services/DataService.cs
@@ -0,0 +1,14 @@
+﻿using CostAccountingAPI.Models;
+
+namespace CostAccountingAPI.Services;
+
+public class DataService : IDataService
+{
+    private List<User> _users = new List<User>();
+    private List<Category> _categories = new List<Category>();
+    private List<Record> _records = new List<Record>();
+    
+    public List<User> Users => _users;
+    public List<Category> Categories => _categories;
+    public List<Record> Records => _records;
+}
